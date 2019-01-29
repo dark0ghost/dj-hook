9779cb975c5753b0c4a3e159c3c95e3682357414
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-01-29 20:44:45 +0300

    Update views.py

--- localibrary/API/views.py
+++ localibrary/API/views.py
@@ -34,6 +34,43 @@ class sendmail:
         self.mail.sendmail(self.send_mail, self.from_mail, self.text)
         self.mail.quit()
 
+
 def post_fon(request):
     return render(request,'api/index.html',{})
+
+
+def save_exel(request):
+ if request.method == "GET":
+   temp= request.GET.get("val")
+   lux= request.GET.get("lux")
+   df = pd.DataFrame({'LUX': [lux]},{"temp":[temp]})
+   writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
+   df.to_excel(writer, sheet_name='Sheet1')
+   writer.save()
+   logging.info("Informational message")
+   return HttpResponse('complite')
+
+def list_push(request):
+    if request.method == "GET":
+        temp = request.GET.get("val")
+        lux = request.GET.get("lux")
+        temp_list.append(temp)
+        lux_list.append(lux)
+        return HttpResponse('save')
+def deleted_list(request):
+    key = request.GET.get("key")
+    if key == "ghostnetwork":
+        temp_list,lux_list=[],[]
+        return HttpResponse('deletade')
+
+def push(request):
+    mail = request.GET.get("mail")
+    key = request.GET.get("key")
+    if key == "String":
+     send = sendmail(from_mail=mail,text=f"{temp_list} && {lux_list}")
+     send.sendfrom()
+     logging.error("error")
+     return HttpResponse("push")
+    else:
+        return HttpResponse("500")
 
