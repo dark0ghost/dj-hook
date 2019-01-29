70565f5771a316b8c72a1917eee30d61fe35e6ed
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-01-29 20:45:27 +0300

    Update urls.py

--- localibrary/API/urls.py
+++ localibrary/API/urls.py
@@ -4,8 +4,13 @@ from . import views
 urlpatterns =[
     path("send/",views.hook_get, name= 'api_get'),
     path("", views.post_fon, name='fon_post'),
+    path ("temp/",views.save_exel,name="pd"),
+    path("save/", views.list_push, name="save"),
+    path("deleted/", views.list_push, name="deleted"),
+    path("send_push/",views.push,name="push"),
 
 ]
+
 
 
 
